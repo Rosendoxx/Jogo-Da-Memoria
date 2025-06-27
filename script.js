f672d430e077a295b8e8e2ecbd29edac3079bcc3
Rosendoxx <leandrosoft0813@gmail.com>
2025-06-27 17:39:36 -0300

    adicionando modo de jogo com vidas

--- script.js
+++ script.js
@@ -1,47 +1,52 @@
-let modoDeJogo;
-let quantidadeDeCartas;
-let lobby = document.querySelector("#cartaslobby");
-let aviso = document.getElementById("aviso");
+let modoDeJogo
+let quantidadeDeCartas
+let vidas
+let vidasHUD = document.getElementById("vidas")
+let lobby = document.querySelector("#cartaslobby")
+let aviso = document.getElementById("aviso")
 let cartasViradas = []
+let encontradas = []
 let imagens = ["images/pikachu.png", "images/bulbassaur.png", "images/charmander.png", "images/squirtle.png", "images/cartepie.png", "images/weedle.png", "images/pidgey.png", "images/rattata.png", "images/sandshrew.png", "images/butterfree.png", "images/clefairy.png", "images/vulpix.png", "images/jigglypuff.png", "images/zubat.png", "images/oddish.png", "images/paras.png", "images/venonat.png", "images/diglett.png", "images/meowth.png", "images/psyduck.png"]
-let pokemons1 = ["Pikachu", "Bulbasaur", "Charmander", "Squirtle", "Caterpie"];
-let pokemons2 = ["Pikachu", "Bulbasaur", "Charmander", "Squirtle", "Caterpie", "Weedle", "Pidgey", "Rattata", "Sandshrew", "ButterFree"];
-let pokemons3 = ["Pikachu", "Bulbasaur", "Charmander", "Squirtle", "Caterpie", "Weedle", "Pidgey", "Rattata", "Sandshrew", "ButterFree", "Clefairy", "Vulpix", "Jigglypuff", "Zubat", "Oddish", "Paras", "Venonat", "Diglett", "Meowth", "Psyduck"];
+let pokemons1 = ["Pikachu", "Bulbasaur", "Charmander", "Squirtle", "Caterpie"]
+let pokemons2 = ["Pikachu", "Bulbasaur", "Charmander", "Squirtle", "Caterpie", "Weedle", "Pidgey", "Rattata", "Sandshrew", "ButterFree"]
+let pokemons3 = ["Pikachu", "Bulbasaur", "Charmander", "Squirtle", "Caterpie", "Weedle", "Pidgey", "Rattata", "Sandshrew", "ButterFree", "Clefairy", "Vulpix", "Jigglypuff", "Zubat", "Oddish", "Paras", "Venonat", "Diglett", "Meowth", "Psyduck"]
 function modo(x){
     if (x==1){
-        modoDeJogo="normal";
+        modoDeJogo="normal"
     }
     else if(x==2){
-        modoDeJogo="com vidas";
+        modoDeJogo="com vidas"
     }
     else if(x==3){
-        modoDeJogo="com tempo";
+        modoDeJogo="com tempo"
     }
     return modoDeJogo
 }
 function quantidade(y){
     if(y==1){
-        quantidadeDeCartas=5;
+        quantidadeDeCartas=5
+        vidas=7
     }
     if(y==2){
-        quantidadeDeCartas=10;
+        quantidadeDeCartas=10
+        vidas=13
     }
     if(y==3){
-        quantidadeDeCartas=20;
+        quantidadeDeCartas=20
+        vidas=15
     }
-    return quantidadeDeCartas;
+    return quantidadeDeCartas
 }
 function criarCartas(){
     if (!modoDeJogo || !quantidadeDeCartas) {
-        aviso.textContent = "Escolha o modo de jogo e a quantidade de cartas!";
+        aviso.textContent = "Escolha o modo de jogo e a quantidade de cartas!"
         setTimeout(()=>{aviso.style.display="none"}, 2000)
-        return;
+        return
     }
-
-    lobby.innerHTML="";
-    lobby.style.display="flex";
+    lobby.innerHTML=""
+    lobby.style.display="flex"
     for (let i = 0; i < quantidadeDeCartas; i++) {
-        let carta1, carta2;
+        let carta1, carta2
         if(quantidadeDeCartas==5){
             carta1 = 
                 `<div class="cartas" data-nome="${pokemons1[i]}" onclick="virarCarta(this)">
@@ -90,21 +95,21 @@ function criarCartas(){
                     </div>
                 </div>`
         }
-        lobby.innerHTML += carta1 + carta2;
+        lobby.innerHTML += carta1 + carta2
     }
 }
 function embaralhar(array){
-    var m=array.length , i , t;
+    var m=array.length , i , t
     while (m){
-        i=Math.floor(Math.random()* m--);
-        t = array[m];
-        array[m] = array[i];
-        array[i] = t;
+        i=Math.floor(Math.random()* m--)
+        t = array[m]
+        array[m] = array[i]
+        array[i] = t
     }
-    return array;
+    return array
 }
 function virarCarta(carta){
-    carta.classList.toggle("virada");
+    carta.classList.toggle("virada")
 }
 function clicou(e){
     let carta = e.currentTarget
@@ -115,11 +120,20 @@ function clicou(e){
             let carta2 = cartasViradas[1].dataset.nome
             if(carta1 === carta2){
                 cartasViradas.forEach(carta =>{
+                    carta.classList.remove("errada")
                     carta.classList.add("encontrada")
                     carta.setAttribute("onclick", "")
                     carta.removeEventListener("click", clicou)
                     cartasViradas = []
+                    encontradas.push(carta)
                 })
+                if(encontradas.length===(quantidadeDeCartas*2)){
+                    aviso.textContent = "Parabéns, você capturou todos os pokémons"
+                    aviso.style.display = "block"
+                    setTimeout(() => {
+                        aviso.style.display="none"
+                    },6000)
+}
             }
             else{
                 cartasViradas.forEach(carta =>{if(carta.classList.contains("errada")){carta.classList.remove("errada")}})
@@ -130,21 +144,48 @@ function clicou(e){
                     })
                     cartasViradas = []
                 }, 1000)
+                --vidas
+                let coracoesDOM = vidasHUD.querySelectorAll(".coracoes")
+                if(vidas>0){coracoesDOM[coracoesDOM.length - 1].remove()}
+                if(vidas==0){
+                    aviso.textContent="Suas vidas acabaram"
+                    aviso.style.display="block"
+                    let cartas = Array.from(document.querySelectorAll(".cartas"))
+                    cartas.forEach(carta => {
+                        carta.removeEventListener("click", clicou)
+                    })
+                    setTimeout(() => {aviso.style.display="none"}, 6000)
+                }
             }
         }
     }
 }
 function jogar() {
-    let cartas = Array.from(document.querySelectorAll(".cartas"));
+    if(modoDeJogo==="com vidas"){
+        vidasHUD.innerHTML = ""
+        for(let i=0; i<vidas; i++){
+            let coracao = document.createElement("img")
+            coracao.classList.add("coracoes")
+            coracao.src= `images/vidas.png`
+            vidasHUD.appendChild(coracao)
+        }
+    }
+    else if(modoDeJogo==="com tempo"){
+        let tempoHUD = document.getElementById("tempo")
+        tempoHUD.innerHTML=`
+        
+        `
+    }
+    let cartas = Array.from(document.querySelectorAll(".cartas"))
     cartas.forEach(carta => {
-        carta.classList.remove("encontrada");
+        carta.classList.remove("encontrada")
         virarCarta(carta)
-    });
-    let cartasEmbaralhadas = embaralhar(cartas);
-    lobby.innerHTML = "";
+    })
+    let cartasEmbaralhadas = embaralhar(cartas)
+    lobby.innerHTML = ""
     cartasEmbaralhadas.forEach(carta => {
         lobby.appendChild(carta);
-        carta.addEventListener("click", clicou);
-    });
+        carta.addEventListener("click", clicou)
+    })
     cartasViradas = [];
 }
